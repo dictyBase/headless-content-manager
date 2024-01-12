5b4e7a40b933f7d14bc65ed3c93051ca03365844
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2024-01-12 08:42:46 -0600

    refactor(index.ts): convert synchronous actions to asynchronous for better performance
    The actions in the command-line interface (CLI) commands have been
    converted from synchronous to asynchronous. This change allows the CLI
    to handle multiple requests concurrently, improving the overall
    performance and responsiveness of the application. The parse method has
    also been changed to parseAsync to support the asynchronous actions.

--- index.ts
+++ index.ts
@@ -12,37 +12,36 @@ program
   .description("CLI to convert slatejs json to lexical format")
   .version("1.0.0")
 
-
 program
   .command("slate-to-html")
   .description("convert slatejs format to html")
   .requiredOption("-f, --file <input>", "input file in slatejs format")
-  .action((options) => {
-    console.log(slateToHtml(options.file))
+  .action(async (options) => {
+    console.log(await slateToHtml(options.file))
   })
 
 program
   .command("slate-to-lexical")
   .description("convert slatejs to lexical format")
   .requiredOption("-f, --file <input>", "input file in slatejs format")
-  .action((options) => {
-    console.log(slateToLexical(options.file))
+  .action(async (options) => {
+    console.log(await slateToLexical(options.file))
   })
 
 program
   .command("legacy-slate-to-html")
   .description("convert legacy slatejs format to html")
   .requiredOption("-f, --file <input>", "input file in legacy slatejs format")
-  .action((options) => {
-    console.log(legacySlateToHtml(options.file))
+  .action(async (options) => {
+    console.log(await legacySlateToHtml(options.file))
   })
 
 program
   .command("legacy-slate-to-lexical")
   .description("convert legacy slatejs to lexical format")
   .requiredOption("-f, --file <input>", "input file in legacy slatejs format")
-  .action((options) => {
-    console.log(legacySlateToLexical(options.file))
+  .action(async (options) => {
+    console.log(await legacySlateToLexical(options.file))
   })
 
-program.parse()
+await program.parseAsync()
