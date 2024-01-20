1f9451f9472efd1c124aaae3986f80bb6b27f181
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2024-01-20 11:51:32 -0600

    feat(converter.ts): add batch conversion functionality for JSON to HTML
    The batchSlateToHtml function was added to allow batch conversion of
    JSON files to HTML. This function reads all JSON files in a specified
    input directory, converts them to HTML using the slateToHtml function,
    and writes the HTML output to a specified output directory. This change
    was made to improve efficiency when dealing with multiple JSON files
    that need to be converted to HTML.

--- src/converter.ts
+++ src/converter.ts
@@ -4,6 +4,8 @@ import { map as Amap } from "fp-ts/Array"
 import { extractContent, curriedBlockToElements } from "./extract"
 import { syncEditor, editorInstance } from "./editor"
 import { type ElementTypeProperties } from "./types"
+import { readdir, writeFile } from "node:fs/promises"
+import { join, parse, format } from "path"
 
 const curriedAddBlockElement =
   (document: Document) => (elem: ElementTypeProperties | null) =>
@@ -37,4 +39,23 @@ const slateToHtml = async (input: string) => {
   return dom.serialize()
 }
 
-export { slateToLexical, slateToHtml }
+const allHtmls = async (folder: string) => {
+  const allFiles = await readdir(folder)
+  return allFiles
+    .filter((file) => file.endsWith(".json"))
+    .map(async (file) => {
+      const inputFile = join(folder, file)
+      const html = await slateToHtml(inputFile)
+      return { html, file }
+    })
+}
+
+const batchSlateToHtml = async (input: string, output: string) => {
+  const allContents = await Promise.all(await allHtmls(input))
+  allContents.map(async ({ html, file }) => {
+    const parsedFile = parse(file)
+    await writeFile(join(output, `${parsedFile.name}.html`), html)
+  })
+}
+
+export { slateToLexical, slateToHtml, batchSlateToHtml }
