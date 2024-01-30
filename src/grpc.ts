9fdc766ee82bc52f0e7bdf8e309729a1bc61b183
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2024-01-30 11:26:55 -0600

    feat(grpc.ts): add StockService client for gRPC communication
    The StockService client is added to enable gRPC communication with the
    stock service. This allows the application to interact with the stock
    service using gRPC, which is a high-performance, open-source universal
    RPC framework.

--- src/grpc.ts
+++ src/grpc.ts
@@ -1,10 +1,16 @@
 import { createPromiseClient, type PromiseClient } from "@connectrpc/connect"
 import { createGrpcTransport } from "@connectrpc/connect-node"
 import { ContentService } from "./es/dictybase/content/content_connect"
+import { StockService } from "./es/dictybase/stock/stock_connect"
 import { tryCatch, tapIO } from "fp-ts/TaskEither"
 import { toError } from "fp-ts/Either"
 import * as Console from "fp-ts/Console"
 import { type LoadContentProperties } from "./types"
+
+const stockClient = (url: string) => {
+  const transport = createGrpcTransport({ baseUrl: url, httpVersion: "2" })
+  return createPromiseClient(StockService, transport)
+}
 
 const contentClient = (url: string) => {
   const transport = createGrpcTransport({ baseUrl: url, httpVersion: "2" })
@@ -34,4 +40,11 @@ const errLogger = tapIO(Console.error)
 const infoLogger = tapIO(Console.info)
 const warnLogger = tapIO(Console.warn)
 
-export { contentClient, loadContent, errLogger, infoLogger, warnLogger }
+export {
+  contentClient,
+  loadContent,
+  errLogger,
+  infoLogger,
+  warnLogger,
+  stockClient,
+}
