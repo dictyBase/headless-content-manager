31e103b2623107efd0772a1352eab2b3e30aa0dc
Kevin Tun <kevin.tun@northwestern.edu>
2026-08-26 08:51:48 -0500

    Add node types

---
+++ src/nodes/WidthTableNode.ts
@@ -0,0 +1,69 @@
+import { TableNode, type SerializedTableNode } from "@lexical/table";
+import { addClassNamesToElement } from "@lexical/utils";
+import type { NodeKey, EditorConfig } from "lexical";
+
+export interface SerializedWidthTableNode extends SerializedTableNode {
+  width: number;
+}
+
+class WidthTableNode extends TableNode {
+  /** @private */
+  __width;
+
+  /**
+   * Returns the node type
+   *
+   * @static
+   */
+  static override getType() {
+    return "table";
+  }
+
+  /**
+   * Returns a new instance of WidthTableNode with the same width and optional key as the input node.
+   *
+   * @static
+   * @param node - The node to clone.
+   */
+  static override clone({ __width, __key }: WidthTableNode): WidthTableNode {
+    return new WidthTableNode(__width, __key);
+  }
+
+  /**
+   * Creates a new instance of WidthTableNode with the specified width and optional key.
+   *
+   * @param width - The width of the table.
+   * @param key - key for the node.
+   */
+  constructor(width: number = 400, key?: NodeKey) {
+    super(key);
+    this.__width = width;
+  }
+
+  /**
+   * Returns a serialized version of the WidthTableNode, including its key and width.
+   *
+   * @override
+   */
+  override exportJSON() {
+    return {
+      ...super.exportJSON(),
+      width: this.__width,
+    };
+  }
+
+  /**
+   * Creates and returns a new HTML table element with the appropriate width and CSS classes.
+   *
+   * @override
+   * @param config - The configuration of the editor.
+   */
+  override createDOM(config: EditorConfig) {
+    const tableElement = document.createElement("table");
+    addClassNamesToElement(tableElement, config.theme.table);
+    tableElement.style.width = `${this.__width}px`;
+    return tableElement;
+  }
+}
+
+export { WidthTableNode };
